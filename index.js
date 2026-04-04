0a02322b22d6709db9a2b758f766073b7381708e
Astik Sharma <astiksharma26@gmail.com>
2026-04-04 18:20:02 +0530

    Final commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import summaryRoutes from './routes/summaryRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
